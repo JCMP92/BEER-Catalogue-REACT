609e49dfc4c82174ae9fa6bd0b5dd12eff6706df
JCMP92 <arq_juanmp@outlook.com>
2022-08-25 11:47:14 -0500

    API has some images missing, use a img url

--- src/Components/BeerCard.js
+++ src/Components/BeerCard.js
@@ -8,7 +8,15 @@ function Beer(props) {
         <Link to={`/catalogue/${props.beer.name}`}>{props.beer.name}</Link>
       </h3>
       <Link to={`/catalogue/${props.beer.name}`}>
-        <img src={props.beer.image_url} height="100px" alt={props.beer.name} />
+        <img
+          src={
+            props.beer.image_url
+              ? props.beer.image_url
+              : 'https://images.punkapi.com/v2/keg.png'
+          }
+          height="100px"
+          alt={props.beer.name}
+        />
       </Link>
       <h4>Alchohol by volume: {props.beer.abv}ABV</h4>
       <p>{props.beer.tagline}</p>
